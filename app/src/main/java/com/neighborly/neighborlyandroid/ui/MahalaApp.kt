d8e7179d8f2c5f44fca8dfa62a1fe4c92113924d
dmitrie-ai <dmitriebivol@gmail.com>
2025-01-20 20:33:38 +0000

    Refactoring

--- app/src/main/java/com/neighborly/neighborlyandroid/ui/MahalaApp.kt
+++ app/src/main/java/com/neighborly/neighborlyandroid/ui/MahalaApp.kt
@@ -1,48 +1,29 @@
 package com.neighborly.neighborlyandroid.ui
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
-import androidx.navigation.compose.composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
+
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.neighborly.neighborlyandroid.ui.inventory.add.AddInventoryViewModel
-import com.neighborly.neighborlyandroid.ui.inventory.view.ViewInventoryViewModel
-import com.neighborly.neighborlyandroid.ui.login.LoginScreen
-import com.neighborly.neighborlyandroid.ui.market.MarketScreen
+
 import com.neighborly.neighborlyandroid.ui.navigation.MahalaNavHost
 import com.neighborly.neighborlyandroid.ui.navigation.isRouteInHierarchy
-import com.neighborly.neighborlyandroid.ui.register.RegistrationScreen
-import kotlinx.coroutines.launch
 
 val LocalSnackbarHostState = compositionLocalOf<SnackbarHostState>{
     error("No Snackbar Host State")
